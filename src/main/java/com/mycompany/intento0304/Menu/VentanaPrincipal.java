e2501bc65314b6ce621eedcf8b726a9e73d63b85
Kristina <Kristina@LAPTOP-AES15J72>
2023-01-05 12:52:57 -0500

    medio

--- src/main/java/com/mycompany/intento0304/Menu/VentanaPrincipal.java
+++ src/main/java/com/mycompany/intento0304/Menu/VentanaPrincipal.java
@@ -2,8 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.intento0304.vista;
+package com.mycompany.intento0304.Menu;
 
+import com.mycompany.intento0304.Menu.Pedido.PedidoEliminar;
+import com.mycompany.intento0304.Menu.Pedido.PedidoInsertar;
+import com.mycompany.intento0304.Menu.Pedido.PedidoModificar;
+import com.mycompany.intento0304.Menu.Producto.ProductoInsertar;
+import com.mycompany.intento0304.vista.*;
 import javax.swing.JFrame;
 
 /**
@@ -18,17 +23,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     public VentanaPrincipal() {
         initComponents();
         
-        var ventanaPedido = new VentanaPedido();
-        var ventanaProducto = new VentanaProducto();
-        var ventanaProveedor = new VentanaProveedor();
-        
-        ventanaPedido.setVisible(true);
-        ventanaProducto.setVisible(true);
-        ventanaProveedor.setVisible(true);
-        
-        this.jDesktopPane1.add(ventanaPedido);
-        this.jDesktopPane1.add(ventanaProducto);
-        this.jDesktopPane1.add(ventanaProveedor);
+       
         
     }
 
@@ -50,14 +45,14 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         jMenu8 = new javax.swing.JMenu();
         jDesktopPane1 = new javax.swing.JDesktopPane();
         jMenuBar3 = new javax.swing.JMenuBar();
-        jMenu5 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu10 = new javax.swing.JMenu();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem5 = new javax.swing.JMenuItem();
-        jMenuItem6 = new javax.swing.JMenuItem();
+        InsertarProducto = new javax.swing.JMenuItem();
+        ModificarProducto = new javax.swing.JMenuItem();
+        EliminarProducto = new javax.swing.JMenuItem();
+        jMenu5 = new javax.swing.JMenu();
+        InsertarPedido = new javax.swing.JMenuItem();
+        ModificarPedido = new javax.swing.JMenuItem();
+        EliminarPedido = new javax.swing.JMenuItem();
         jMenu6 = new javax.swing.JMenu();
         jMenuItem7 = new javax.swing.JMenuItem();
         jMenuItem8 = new javax.swing.JMenuItem();
@@ -87,34 +82,54 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         );
         jDesktopPane1Layout.setVerticalGroup(
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 388, Short.MAX_VALUE)
+            .addGap(0, 389, Short.MAX_VALUE)
         );
-
-        jMenu5.setText("Pedido");
-
-        jMenuItem1.setText("Insertar Datos");
-        jMenu5.add(jMenuItem1);
-
-        jMenuItem2.setText("Modificar Datos");
-        jMenu5.add(jMenuItem2);
-
-        jMenuItem3.setText("Eliminar Datos");
-        jMenu5.add(jMenuItem3);
-
-        jMenuBar3.add(jMenu5);
 
         jMenu10.setText("Producto");
 
-        jMenuItem4.setText("Insertar Datos");
-        jMenu10.add(jMenuItem4);
+        InsertarProducto.setText("Insertar Datos");
+        InsertarProducto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InsertarProductoActionPerformed(evt);
+            }
+        });
+        jMenu10.add(InsertarProducto);
 
-        jMenuItem5.setText("Modificar Datos");
-        jMenu10.add(jMenuItem5);
+        ModificarProducto.setText("Modificar Datos");
+        jMenu10.add(ModificarProducto);
 
-        jMenuItem6.setText("Eliminar Datos");
-        jMenu10.add(jMenuItem6);
+        EliminarProducto.setText("Eliminar Datos");
+        jMenu10.add(EliminarProducto);
 
         jMenuBar3.add(jMenu10);
+
+        jMenu5.setText("Pedido");
+
+        InsertarPedido.setText("Insertar Datos");
+        InsertarPedido.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InsertarPedidoActionPerformed(evt);
+            }
+        });
+        jMenu5.add(InsertarPedido);
+
+        ModificarPedido.setText("Modificar Datos");
+        ModificarPedido.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ModificarPedidoActionPerformed(evt);
+            }
+        });
+        jMenu5.add(ModificarPedido);
+
+        EliminarPedido.setText("Eliminar Datos");
+        EliminarPedido.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                EliminarPedidoActionPerformed(evt);
+            }
+        });
+        jMenu5.add(EliminarPedido);
+
+        jMenuBar3.add(jMenu5);
 
         jMenu6.setText("Proveedor");
 
@@ -145,6 +160,38 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void InsertarPedidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InsertarPedidoActionPerformed
+       var pedidoInsertar= new PedidoInsertar();
+        pedidoInsertar.setClosable(true);
+        pedidoInsertar.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        pedidoInsertar.setVisible(true);
+        this.jDesktopPane1.add(pedidoInsertar); 
+    }//GEN-LAST:event_InsertarPedidoActionPerformed
+
+    private void ModificarPedidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ModificarPedidoActionPerformed
+         var pedidoModificar= new PedidoModificar();
+        pedidoModificar.setClosable(true);
+        pedidoModificar.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        pedidoModificar.setVisible(true);
+        this.jDesktopPane1.add(pedidoModificar); 
+    }//GEN-LAST:event_ModificarPedidoActionPerformed
+
+    private void EliminarPedidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EliminarPedidoActionPerformed
+         var pedidoEliminar= new PedidoEliminar();
+        pedidoEliminar.setClosable(true);
+        pedidoEliminar.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        pedidoEliminar.setVisible(true);
+        this.jDesktopPane1.add(pedidoEliminar); 
+    }//GEN-LAST:event_EliminarPedidoActionPerformed
+
+    private void InsertarProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InsertarProductoActionPerformed
+         var productoInsertar= new ProductoInsertar();
+        productoInsertar.setClosable(true);
+        productoInsertar.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        productoInsertar.setVisible(true);
+        this.jDesktopPane1.add(productoInsertar); 
+    }//GEN-LAST:event_InsertarProductoActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -171,6 +218,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -181,6 +229,12 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem EliminarPedido;
+    private javax.swing.JMenuItem EliminarProducto;
+    private javax.swing.JMenuItem InsertarPedido;
+    private javax.swing.JMenuItem InsertarProducto;
+    private javax.swing.JMenuItem ModificarPedido;
+    private javax.swing.JMenuItem ModificarProducto;
     private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu10;
@@ -193,12 +247,6 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar2;
     private javax.swing.JMenuBar jMenuBar3;
     private javax.swing.JMenuBar jMenuBar4;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
-    private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JMenuItem jMenuItem7;
     private javax.swing.JMenuItem jMenuItem8;
     private javax.swing.JMenuItem jMenuItem9;
--- src/main/java/com/mycompany/intento0304/vista/VentanaPedido.java
+++ src/main/java/com/mycompany/intento0304/vista/VentanaPedido.java
@@ -258,9 +258,7 @@ public class VentanaPedido extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
